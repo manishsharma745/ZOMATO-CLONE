6620ce0479842badffdc7268e8e0ba06127ef431
Manish Sharma <manish745sharma@gmail.com>
2022-09-03 22:56:30 +0530

    First commit

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Made with 🔥 by Manish Sharma</div>
+  );
+};
+
+export default Footer;
